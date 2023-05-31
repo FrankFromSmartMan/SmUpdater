b9fc0046db980b4201423a41ad61b9354c364d40
Frank <frankhuang66655@gmail.com>
2023-05-31 14:44:27 +0800

    Add project files.

---
+++ ExeUpdater/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace ExeUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btnUpdate = new Button();
+            progressBar = new ProgressBar();
+            lstStatus = new ListBox();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tbLastUpdatedTime = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            tabPage2 = new TabPage();
+            label7 = new Label();
+            tbFileCount = new TextBox();
+            btnPaste = new Button();
+            btnAdd = new Button();
+            btnRemove = new Button();
+            tbFileName = new TextBox();
+            label6 = new Label();
+            lstFiles = new ListBox();
+            tbLastSavedTime = new TextBox();
+            label3 = new Label();
+            tbRemotePath = new TextBox();
+            btnPathBrowse = new Button();
+            btnSave = new Button();
+            tbLocalPath = new TextBox();
+            label5 = new Label();
+            lbRemotePath = new Label();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.BackColor = Color.Transparent;
+            btnUpdate.Location = new Point(24, 18);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(119, 31);
+            btnUpdate.TabIndex = 0;
+            btnUpdate.Text = "更新";
+            btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // progressBar
+            // 
+            progressBar.Location = new Point(24, 91);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(744, 29);
+            progressBar.TabIndex = 1;
+            // 
+            // lstStatus
+            // 
+            lstStatus.FormattingEnabled = true;
+            lstStatus.HorizontalScrollbar = true;
+            lstStatus.ItemHeight = 20;
+            lstStatus.Location = new Point(24, 129);
+            lstStatus.Name = "lstStatus";
+            lstStatus.SelectionMode = SelectionMode.MultiExtended;
+            lstStatus.Size = new Size(744, 364);
+            lstStatus.TabIndex = 3;
+            lstStatus.KeyDown += lstStatus_KeyDown;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Dock = DockStyle.Fill;
+            tabControl1.Location = new Point(0, 0);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(800, 540);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(tbLastUpdatedTime);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(lstStatus);
+            tabPage1.Controls.Add(btnUpdate);
+            tabPage1.Controls.Add(progressBar);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(792, 507);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "程式更新";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tbLastUpdatedTime
+            // 
+            tbLastUpdatedTime.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            tbLastUpdatedTime.Location = new Point(541, 22);
+            tbLastUpdatedTime.Name = "tbLastUpdatedTime";
+            tbLastUpdatedTime.ReadOnly = true;
+            tbLastUpdatedTime.Size = new Size(227, 25);
+            tbLastUpdatedTime.TabIndex = 6;
+            tbLastUpdatedTime.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(430, 25);
+            label2.Name = "label2";
+            label2.Size = new Size(105, 20);
+            label2.TabIndex = 5;
+            label2.Text = "上次更新時間";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 68);
+            label1.Name = "label1";
+            label1.Size = new Size(41, 20);
+            label1.TabIndex = 4;
+            label1.Text = "狀態";
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(label7);
+            tabPage2.Controls.Add(tbFileCount);
+            tabPage2.Controls.Add(btnPaste);
+            tabPage2.Controls.Add(btnAdd);
+            tabPage2.Controls.Add(btnRemove);
+            tabPage2.Controls.Add(tbFileName);
+            tabPage2.Controls.Add(label6);
+            tabPage2.Controls.Add(lstFiles);
+            tabPage2.Controls.Add(tbLastSavedTime);
+            tabPage2.Controls.Add(label3);
+            tabPage2.Controls.Add(tbRemotePath);
+            tabPage2.Controls.Add(btnPathBrowse);
+            tabPage2.Controls.Add(btnSave);
+            tabPage2.Controls.Add(tbLocalPath);
+            tabPage2.Controls.Add(label5);
+            tabPage2.Controls.Add(lbRemotePath);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(792, 507);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "參數設定";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(649, 440);
+            label7.Name = "label7";
+            label7.Size = new Size(73, 20);
+            label7.TabIndex = 15;
+            label7.Text = "檔案數量";
+            // 
+            // tbFileCount
+            // 
+            tbFileCount.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            tbFileCount.Location = new Point(649, 463);
+            tbFileCount.Name = "tbFileCount";
+            tbFileCount.ReadOnly = true;
+            tbFileCount.RightToLeft = RightToLeft.Yes;
+            tbFileCount.Size = new Size(119, 25);
+            tbFileCount.TabIndex = 14;
+            tbFileCount.TabStop = false;
+            // 
+            // btnPaste
+            // 
+            btnPaste.BackColor = Color.Transparent;
+            btnPaste.Location = new Point(649, 281);
+            btnPaste.Name = "btnPaste";
+            btnPaste.Size = new Size(119, 31);
+            btnPaste.TabIndex = 7;
+            btnPaste.Text = "貼上";
+            btnPaste.UseVisualStyleBackColor = false;
+            btnPaste.Click += btnPaste_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.BackColor = Color.Transparent;
+            btnAdd.Location = new Point(648, 206);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(119, 31);
+            btnAdd.TabIndex = 5;
+            btnAdd.Text = "加入";
+            btnAdd.UseVisualStyleBackColor = false;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnRemove
+            // 
+            btnRemove.BackColor = Color.Transparent;
+            btnRemove.Location = new Point(649, 244);
+            btnRemove.Name = "btnRemove";
+            btnRemove.Size = new Size(119, 31);
+            btnRemove.TabIndex = 6;
+            btnRemove.Text = "移除";
+            btnRemove.UseVisualStyleBackColor = false;
+            btnRemove.Click += btnRemove_Click;
+            // 
+            // tbFileName
+            // 
+            tbFileName.Font = new Font("Consolas", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            tbFileName.Location = new Point(24, 210);
+            tbFileName.Name = "tbFileName";
+            tbFileName.Size = new Size(618, 27);
+            tbFileName.TabIndex = 4;
+            tbFileName.KeyPress += tbFileName_KeyPress;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(23, 187);
+            label6.Name = "label6";
+            label6.Size = new Size(73, 20);
+            label6.TabIndex = 12;
+            label6.Text = "檔案名稱";
+            // 
+            // lstFiles
+            // 
+            lstFiles.Font = new Font("Consolas", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            lstFiles.FormattingEnabled = true;
+            lstFiles.ItemHeight = 20;
+            lstFiles.Location = new Point(23, 244);
+            lstFiles.Name = "lstFiles";
+            lstFiles.SelectionMode = SelectionMode.MultiExtended;
+            lstFiles.Size = new Size(619, 244);
+            lstFiles.TabIndex = 11;
+            lstFiles.KeyDown += lstFiles_KeyDown;
+            // 
+            // tbLastSavedTime
+            // 
+            tbLastSavedTime.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            tbLastSavedTime.Location = new Point(541, 22);
+            tbLastSavedTime.Name = "tbLastSavedTime";
+            tbLastSavedTime.ReadOnly = true;
+            tbLastSavedTime.Size = new Size(227, 25);
+            tbLastSavedTime.TabIndex = 10;
+            tbLastSavedTime.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(430, 25);
+            label3.Name = "label3";
+            label3.Size = new Size(105, 20);
+            label3.TabIndex = 9;
+            label3.Text = "上次儲存時間";
+            // 
+            // tbRemotePath
+            // 
+            tbRemotePath.Font = new Font("Consolas", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            tbRemotePath.Location = new Point(24, 91);
+            tbRemotePath.Name = "tbRemotePath";
+            tbRemotePath.Size = new Size(743, 27);
+            tbRemotePath.TabIndex = 1;
+            // 
+            // btnPathBrowse
+            // 
+            btnPathBrowse.BackColor = Color.Transparent;
+            btnPathBrowse.Location = new Point(648, 147);
+            btnPathBrowse.Name = "btnPathBrowse";
+            btnPathBrowse.Size = new Size(119, 31);
+            btnPathBrowse.TabIndex = 3;
+            btnPathBrowse.Text = "瀏覽";
+            btnPathBrowse.UseVisualStyleBackColor = false;
+            btnPathBrowse.Click += btnPathBrowse_Click;
+            // 
+            // btnSave
+            // 
+            btnSave.BackColor = Color.Transparent;
+            btnSave.Location = new Point(24, 18);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(119, 31);
+            btnSave.TabIndex = 0;
+            btnSave.Text = "儲存";
+            btnSave.UseVisualStyleBackColor = false;
+            btnSave.Click += btnSave_Click;
+            // 
+            // tbLocalPath
+            // 
+            tbLocalPath.Font = new Font("Consolas", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            tbLocalPath.Location = new Point(24, 151);
+            tbLocalPath.Name = "tbLocalPath";
+            tbLocalPath.Size = new Size(618, 27);
+            tbLocalPath.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(23, 128);
+            label5.Name = "label5";
+            label5.Size = new Size(73, 20);
+            label5.TabIndex = 4;
+            label5.Text = "本機路徑";
+            // 
+            // lbRemotePath
+            // 
+            lbRemotePath.AutoSize = true;
+            lbRemotePath.Location = new Point(23, 68);
+            lbRemotePath.Name = "lbRemotePath";
+            lbRemotePath.Size = new Size(73, 20);
+            lbRemotePath.TabIndex = 0;
+            lbRemotePath.Text = "遠端路徑";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 540);
+            Controls.Add(tabControl1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SmartMan 更新工具";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnUpdate;
+        private ProgressBar progressBar;
+        private ListBox lstStatus;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Label label1;
+        private Label label2;
+        private TextBox tbLastUpdatedTime;
+        private Button btnSave;
+        private TextBox tbLocalPath;
+        private Label label5;
+        private Label lbRemotePath;
+        private Button btnPathBrowse;
+        private TextBox tbRemotePath;
+        private TextBox tbLastSavedTime;
+        private Label label3;
+        private Label label6;
+        private ListBox lstFiles;
+        private Button btnAdd;
+        private Button btnRemove;
+        private TextBox tbFileName;
+        private Button btnPaste;
+        private Label label7;
+        private TextBox tbFileCount;
+    }
+}
---
+++ ExeUpdater/Form1.cs
@@ -0,0 +1,355 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExeUpdater
+{
+    public partial class Form1 : Form
+    {
+        private readonly string logDirectory = "";
+        private readonly string settingsFilePath = "settings.txt";
+        private string LastUpdatedTime
+        {
+            get => tbLastUpdatedTime.Text;
+            set => tbLastUpdatedTime.Text = value;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnUpdate.Click += BtnUpdate_Click;
+            LastUpdatedTime = LoadLastUpdatedTime();
+            LoadSavedData();
+            lstStatus.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            lstStatus.MeasureItem += lst_MeasureItem;
+            lstStatus.DrawItem += lst_DrawItem;
+        }
+
+
+        private void lst_MeasureItem(object? sender, MeasureItemEventArgs e)
+        {
+            e.ItemHeight = (int)e.Graphics.MeasureString(lstStatus.Items[e.Index].ToString(), lstStatus.Font, lstStatus.Width).Height;
+        }
+
+        private void lst_DrawItem(object? sender, DrawItemEventArgs e)
+        {
+            e.DrawBackground();
+            e.DrawFocusRectangle();
+
+            if (e.Index >= 0 && e.Index < lstStatus.Items.Count)
+            {
+                var item = lstStatus.Items[e.Index];
+                if (item != null)
+                {
+                    e.Graphics.DrawString(item.ToString(), e.Font, new SolidBrush(e.ForeColor), e.Bounds);
+                }
+            }
+        }
+        private void SaveLastUpdatedTime(string lastUpdatedTime)
+        {
+            File.WriteAllText(settingsFilePath, lastUpdatedTime);
+        }
+
+        private void LoadSavedData()
+        {
+            string filePath = "saveData.txt";
+            if (File.Exists(filePath))
+            {
+                string savedData = File.ReadAllText(filePath);
+                string[] savedValues = savedData.Split('|');
+
+                if (savedValues.Length >= 4)
+                {
+                    tbRemotePath.Text = string.IsNullOrEmpty(savedValues[0]) ? "https://www.smartmancs.com.tw/Download/Frank/remoteFolder1" : savedValues[0];
+                    tbLocalPath.Text = savedValues[1];
+                    lstFiles.Items.AddRange(savedValues[2].Split(','));
+                    tbLastSavedTime.Text = savedValues[3];
+                    tbFileCount.Text = lstFiles.Items.Count.ToString();
+                }
+            }
+            else
+            {
+                tbRemotePath.Text = "https://www.smartmancs.com.tw/Download/Frank/remoteFolder1";
+            }
+        }
+
+        private async void BtnUpdate_Click(object? sender, EventArgs e)
+        {
+            LastUpdatedTime = DateTime.Now.ToString();
+            SaveLastUpdatedTime(LastUpdatedTime);
+
+            btnUpdate.Enabled = false;
+            progressBar.Value = 0;
+
+            string localPath = tbLocalPath.Text.Trim();
+
+            if (!Directory.Exists(localPath))
+            {
+                LogText("[Error] 設定錯誤，本機路徑不存在");
+                ResetUI();
+                return;
+            }
+
+            LogText("檢查更新...");
+
+            try
+            {
+                using HttpClient client = new HttpClient();
+                string remotePath = tbRemotePath.Text.Trim();
+                int totalFiles = lstFiles.Items.Count;
+                int completedFiles = 0;
+
+                foreach (var item in lstFiles.Items)
+                {
+                    string fileName = item.ToString() ?? "";
+                    string fileUrl = Path.Combine(remotePath, fileName);
+                    string filePath = Path.Combine(tbLocalPath.Text, fileName);
+
+                    HttpResponseMessage response;
+
+                    try
+                    {
+                        response = await client.GetAsync(fileUrl);
+                    }
+                    catch (HttpRequestException ex)
+                    {
+                        LogText($"[Error] 無法下載檔案 '{fileName}': {ex.Message}");
+                        continue;
+                    }
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        LogText($"[Error] 無法下載檔案 '{fileName}'。 回傳錯誤代碼 {(int)response.StatusCode}");
+                        continue;
+                    }
+
+                    try
+                    {
+                        using Stream contentStream = await response.Content.ReadAsStreamAsync();
+                        using FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None);
+
+                        const int bufferSize = 8192;
+                        byte[] buffer = new byte[bufferSize];
+                        int bytesRead;
+                        long totalBytesRead = 0;
+                        long totalBytes = response.Content.Headers.ContentLength ?? 0;
+                        int previousProgressPercentage = 0;
+
+                        while ((bytesRead = await contentStream.ReadAsync(buffer, 0, bufferSize)) > 0)
+                        {
+                            await fileStream.WriteAsync(buffer, 0, bytesRead);
+                            totalBytesRead += bytesRead;
+
+                            int progressPercentage = (int)((totalBytesRead / (double)totalBytes) * 100);
+                            if (progressPercentage - previousProgressPercentage >= 20)
+                            {
+                                progressBar.Invoke((MethodInvoker)(() => progressBar.Value = progressPercentage));
+                                LogText($"下載 '{fileName}': {progressPercentage}%");
+                                previousProgressPercentage = progressPercentage;
+                            }
+                        }
+
+                        completedFiles++;
+                        LogText($"檔案 '{fileName}' 下載儲存成功!");
+                    }
+                    catch (Exception ex)
+                    {
+                        LogText($"[Error] 無法儲存下載後的檔案 '{fileName}': {ex.Message}");
+                    }
+                }
+
+                if (completedFiles == totalFiles)
+                {
+                    LogText("所有檔案下載儲存成功!");
+                }
+                else
+                {
+                    LogText("[Error] 更新失敗: 部分檔案無法下載或儲存");
+                }
+            }
+            catch (Exception ex)
+            {
+                LogText("[Error] 更新失敗: " + ex.Message);
+            }
+            finally
+            {
+                LastUpdatedTime = DateTime.Now.ToString();
+                ResetUI();
+            }
+        }
+
+        private void ResetUI()
+        {
+            btnUpdate.Enabled = true;
+        }
+
+        public void LogText(string message)
+        {
+            string formattedMessage = $"{DateTime.Now:yyyy/MM/dd HH:mm:ss} --- {message}";
+            lstStatus.Items.Add(formattedMessage);
+
+            string logFileName = $"db_exeUpdater_{DateTime.Now:yyyyMMdd}.txt";
+            string logFilePath = string.IsNullOrEmpty(logDirectory) ? logFileName : Path.Combine(logDirectory, logFileName);
+
+            Console.WriteLine(formattedMessage);
+
+            using var logFile = new StreamWriter(logFilePath, true);
+            logFile.WriteLine(formattedMessage);
+        }
+
+        private string LoadLastUpdatedTime()
+        {
+            if (File.Exists(settingsFilePath))
+            {
+                return File.ReadAllText(settingsFilePath);
+            }
+
+            return string.Empty;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            lstFiles.Items.Add(tbFileName.Text);
+            tbFileName.Clear();
+            tbFileCount.Text = lstFiles.Items.Count.ToString();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstFiles.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("請選擇檔案項目", "無檔案選取", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            List<int> selectedIndices = new List<int>();
+
+            for (int i = lstFiles.SelectedIndices.Count - 1; i >= 0; i--)
+            {
+                selectedIndices.Add(lstFiles.SelectedIndices[i]);
+            }
+
+            foreach (int index in selectedIndices)
+            {
+                lstFiles.Items.RemoveAt(index);
+            }
+
+            tbFileCount.Text = lstFiles.Items.Count.ToString();
+        }
+
+        private void btnPathBrowse_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            DialogResult result = folderBrowserDialog.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+            {
+                tbLocalPath.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void tbFileName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+                string fileName = tbFileName.Text.Trim();
+
+                if (!string.IsNullOrWhiteSpace(fileName))
+                {
+                    lstFiles.Items.Add(fileName);
+                    tbFileName.Clear();
+                    tbFileCount.Text = lstFiles.Items.Count.ToString();
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string remotePath = tbRemotePath.Text.Trim();
+            string localPath = tbLocalPath.Text.Trim();
+            List<string> files = new List<string>(lstFiles.Items.Count);
+
+            foreach (var item in lstFiles.Items)
+            {
+                string fileName = item.ToString() ?? "";
+                files.Add(fileName);
+            }
+
+            DateTime currentTime = DateTime.Now;
+
+            string saveData = $"{remotePath}|{localPath}|{string.Join(",", files)}|{currentTime}";
+
+            string filePath = "saveData.txt";
+            File.WriteAllText(filePath, saveData);
+
+            MessageBox.Show("儲存成功", "儲存", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            tbLastSavedTime.Text = currentTime.ToString();
+        }
+
+        private void lstStatus_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var selectedItem in lstStatus.SelectedItems)
+                {
+                    string itemText = selectedItem.ToString() ?? "";
+                    sb.AppendLine(itemText);
+                }
+
+                if (sb.Length > 0)
+                {
+                    Clipboard.SetText(sb.ToString());
+                }
+            }
+        }
+
+        private void lstFiles_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.C)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var selectedItem in lstFiles.SelectedItems)
+                {
+                    string itemText = selectedItem.ToString() ?? "";
+                    sb.AppendLine(itemText);
+                }
+
+                if (sb.Length > 0)
+                {
+                    Clipboard.SetText(sb.ToString());
+                }
+            }
+        }
+
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            string clipboardText = Clipboard.GetText();
+
+            if (string.IsNullOrWhiteSpace(clipboardText))
+            {
+                MessageBox.Show("剪貼簿的內容為空", "剪貼簿為空", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            string[] items = clipboardText.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            int itemCount = items.Length;
+            lstFiles.Items.AddRange(items);
+
+            MessageBox.Show($"{itemCount} 檔案項目已將入至清單", "新增檔案", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            tbFileCount.Text = lstFiles.Items.Count.ToString();
+        }
+    }
+}
